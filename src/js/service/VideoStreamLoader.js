692361238a27f829e17bcd76588f6c1c265084d8
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-04-27 22:12:46 +0900

    reimplemnet VideoStreamLoader

--- src/js/service/VideoStreamLoader.js
+++ src/js/service/VideoStreamLoader.js
@@ -1,31 +1,123 @@
 export default class VideoStreamLoader {
-    constructor() {
-        this._devices = [];
+    /**
+     * loaded video devices
+     *
+     * @type {Array}
+     * @private
+     */
+    _videoDevices = [];
+
+    /**
+     *
+     * @type {number}
+     * @private
+     */
+    _currentDeviceIndex = 0;
+
+    /**
+     *
+     * @type {null}
+     * @private
+     */
+    _currentStream = null;
+
+    /**
+     * target HtmlVideoElement.
+     *
+     * @type {null}
+     * @private
+     */
+    _videoElement = null;
+
+    /**
+     * load video device with {@link navigator}
+     *
+     * @param videoElement
+     * @returns {Promise.<TResult>}
+     * @throws thrown {@link Error} when device didn't provide any video input.
+     */
+    load(videoElement) {
+        this._videoElement = videoElement;
+
+        return Promise.resolve()
+            .then(() => {
+                return this._getVideoDeviceListPromise();
+            })
+            .then((videoDevices) => {
+                this._videoDevices = videoDevices;
+                return this._getVideoStreamPromise();
+            })
+            .then((videoMediaStream) => {
+                this._currentStream = videoMediaStream;
+                return this._getLoadedVideoElementPromise(videoMediaStream);
+            })
+    };
+
+    /**
+     *
+     * @returns {Promise.<TResult>}
+     */
+    changeDevice() {
+        this.stop();
+
+        this._currentDeviceIndex++;
+        this._currentDeviceIndex = this._currentDeviceIndex % this._videoDevices.length;
+
+        return Promise.resolve()
+            .then(() => {
+                return this._getVideoStreamPromise();
+            })
+            .then((videoMediaStream) => {
+                this._currentStream = videoMediaStream;
+                return this._getLoadedVideoElementPromise(videoMediaStream);
+            })
     }
 
-    load() {
+    /**
+     * stop video in use.
+     */
+    stop() {
+        this._currentStream.getVideoTracks()[0].stop();
+        this._currentStream = null;
+    }
+    
+    _getVideoStreamPromise() {
+        const currentDeviceId = this._videoDevices[this._currentDeviceIndex].deviceId;
+
+        return navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: {
+                deviceId: currentDeviceId
+            }
+        });
+    }
+
+    _getVideoDeviceListPromise() {
         return Promise.resolve()
             .then(() => {
                 return navigator.mediaDevices.enumerateDevices();
             })
             .then((mediaDeviceInfoList) => {
-                console.log('使える入出力デバイスs->', mediaDeviceInfoList);
-
                 const videoDevices = mediaDeviceInfoList.filter((deviceInfo) => {
                     return deviceInfo.kind == 'videoinput';
                 }).reverse();
+
                 if (videoDevices.length < 1) {
-                    throw new Error('ビデオの入力デバイスがない、、、、、。');
+                    throw new Error('Could not find any video devices.');
                 }
 
-                this._devices = videoDevices;
-
-                return navigator.mediaDevices.getUserMedia({
-                    audio: false,
-                    video: {
-                        deviceId: videoDevices[0].deviceId
-                    }
+                return new Promise((fulfilled, rejected) => {
+                    fulfilled(videoDevices);
                 });
             });
+    }
+
+    _getLoadedVideoElementPromise(videoMediaStream) {
+        return new Promise((fulfilled, rejected) => {
+            this._videoElement.addEventListener('loadedmetadata', () => {
+                fulfilled();
+            });
+            this._videoElement.srcObject = videoMediaStream;
+        })
     }
 };
